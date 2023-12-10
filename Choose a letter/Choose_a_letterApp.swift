af2c2be6825b457a19b7c59e38da77886b6c4cf3
Thiago Antonio Marafon <tmarafon@gmail.com>
2023-12-09 17:07:50 -0800

    Initial Commit

---
+++ Choose a letter/Choose_a_letterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Choose_a_letterApp.swift
+//  Choose a letter
+//
+//  Created by Thiago Marafon on 09/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Choose_a_letterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Choose a letter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Choose a letter
+//
+//  Created by Thiago Marafon on 09/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
